d9b67527122862a14750c77b3068fafa7ff5abfa
syed akash <syedakashscu@outlook.com>
2024-02-16 12:41:02 +0000

    Pagination

--- projects/utils.py
+++ projects/utils.py
@@ -1,5 +1,30 @@
-from .models import Project, Tag  # Assuming Tag model is also imported here
+from .models import Project, Tag
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+def paginateProjects(request, projects, results):
+    page = request.GET.get('page')
+    paginator = Paginator(projects, results)  # Removed redundant 'results = 3'
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
+
+    leftIndex = (int(page) - 4)
+    if leftIndex < 1:
+        leftIndex = 1
+
+    rightIndex = (int(page) + 5)
+    if rightIndex > paginator.num_pages:
+        rightIndex = paginator.num_pages + 1
+
+    custom_range = range(leftIndex, rightIndex)
+    return custom_range, projects  # Corrected 'cunstom_range' to 'custom_range'
 
 def searchProjects(request):
     search_query = ''
--- projects/views.py
+++ projects/views.py
@@ -2,35 +2,32 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-
 from .models import Project, Tag
 from .forms import ProjectForm
-from .utils import searchProjects
+from .utils import searchProjects, paginateProjects
 
 def projects(request):
-
-
     projects, search_query = searchProjects(request)
-   
+    custom_range, projects = paginateProjects(request, projects, 2)
 
-    context = {'projects': projects, 'search_query': search_query}
-    return render(request,'projects/projects.html',context)
+    context = {'projects': projects, 'search_query': search_query, 'custom_range': custom_range}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
-    print('projectobj:', projectObj)
-    return render(request,'projects/single-project.html',{'project':projectObj, 'tags': tags})
+    print('projectObj:', projectObj)
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'tags': tags})
 
 @login_required(login_url="login")
 def createProject(request):
     profile = request.user.profile
-    form = ProjectForm()  
+    form = ProjectForm()
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            project = form.save (commit=False)
+            project = form.save(commit=False)
             project.owner = profile
             project.save()
             return redirect('account')
@@ -39,16 +36,16 @@ def createProject(request):
     return render(request, "projects/project_form.html", context)
 
 @login_required(login_url="login")
-def updateProject(request,pk):
+def updateProject(request, pk):
     profile = request.user.profile
     project = profile.project_set.get(id=pk)
-    form = ProjectForm(instance=project)  
+    form = ProjectForm(instance=project)
 
     if request.method == 'POST':
-        form = ProjectForm(request.POST,request.FILES, instance=project)
+        form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
-            form.save ()
-        return redirect('account')
+            form.save()
+            return redirect('account')
 
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
